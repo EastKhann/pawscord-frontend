eb5226802f63ef4241cf70a2ef8d9070424eda3d
EastKhann <dogudoguwedogu@gmail.com>
2026-02-05 02:06:32 +0300

    APK+EXE build complete

---
+++ build/static/js/apiEndpoints-DTuZVK56.js
@@ -0,0 +1,1 @@
+const a=()=>"https://api.pawscord.com",e=()=>a().replace("/api","/media"),t=a,p=e;export{p as a,t as g};
---
+++ build/static/js/index-taPPdeYU.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-grPt_ios.js";import"./react-core-DMT_QkRe.js";const r=i("PushNotifications",{});export{r as PushNotifications};
