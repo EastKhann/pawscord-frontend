53de90807b8c9d47af0da7329efed608f6cbbde5
EastKhann <dogudoguwedogu@gmail.com>
2026-02-11 22:17:59 +0300

    APK+AAB+EXE build v1.1.210 complete

--- build/static/js/chart-vendor-DSMCmWDh.js
+++
@@ -1,1 +0,0 @@
-import"./react-core-Cj7FUMmY.js";
---
+++ build/static/js/index-BhYSFoQU.js
@@ -0,0 +1,1 @@
+import{w as i}from"./index-D_6pZib5.js";import"./react-core-B6hadH-M.js";const s=i("PushNotifications",{});export{s as PushNotifications};
